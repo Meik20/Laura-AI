a34895056e8840cd37e73f1cac18df1028dfac69
Meik20 <mbaye.ivan@gmail.com>
2026-05-17 02:58:57 +0100

    fix: express 5 path-to-regexp catch-all compatibility

--- server/index.js
+++ server/index.js
@@ -52,7 +52,7 @@ app.post('/api/chat', async (req, res) => {
 // Serve Frontend in Production
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
